a252e915dfb259bad2271e27cb80daf0fc159e30
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-16 15:35:29 -0600

    added resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,18 +1,41 @@
 import React, {useState, useEffect} from 'react'
-import { Navbar, Footer } from '../component/Component';
+import { Document, Page, pdfjs } from 'react-pdf'
 
+import { Navbar, Footer } from '../component/Component'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 
 export const Resume: React.FC = () => {
     // sets title
     useEffect(() => {
         document.title = "Andrew's Portfolio | Resume"
-    }, []);
+    }, [])
+
+    const removeTextLayerOffset = () => {
+        const textLayers = document.querySelectorAll(".react-pdf__Page__textContent")
+            textLayers.forEach((layer: any) => {
+            const { style } = layer
+            style.top = "0"
+            style.left = "0"
+            style.transform = ""
+        })
+    }
 
     return (
         <>
             <Navbar highlight="resume" />
-            <Footer highlight="resume" />
+            <div className="block">
+                <div className="resumeBody">
+                    <h1>Resume</h1>
+                    <p><a>Direct link</a> to my resume or preview below</p>
+                    <Document file={"resume.pdf"}>
+                        <div style={{paddingLeft: window.innerWidth * 0.07}}>
+                            <Page onLoadSuccess={removeTextLayerOffset} pageNumber={1} width={window.innerWidth * 0.56} />
+                        </div>
+                    </Document>
+                </div>
+                <Footer highlight="resume" />
+            </div>
         </>
     )
 }
