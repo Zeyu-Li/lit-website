6660c560758da38577e84d7e2cf656e9d58cab69
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-15 22:30:02 -0600

    cleaned up navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -14,11 +14,13 @@ export const Navbar: React.FC<Prop> = ({highlight}) => {
     return (
         <nav className="navbar">
             <HashLink to="/"><img src={logo} className="navIcon" />Andrew's Portfolio</HashLink>
-            <HashLink to="/" className={highlight === 'home' ? "highlightedNav": ""}>Home</HashLink>
-            <HashLink to="/#projects" className={highlight === 'projects' ? "highlightedNav": ""}>Projects</HashLink>
-            <HashLink to="/#contact" className={highlight === 'contact' ? "highlightedNav": ""}>Contact</HashLink>
-            <HashLink to="/resume" className={highlight === 'resume' ? "highlightedNav": ""}>Resume</HashLink>
-            <a target="_blank" href="https://andrew-li-blog.vercel.app/">Blog</a>
+            <div className="navItems">
+                <HashLink to="/" className={highlight === 'home' ? "highlightedNav": ""}>Home</HashLink>
+                <HashLink to="/#projects" className={highlight === 'projects' ? "highlightedNav": ""}>Projects</HashLink>
+                <HashLink to="/#contact" className={highlight === 'contact' ? "highlightedNav": ""}>Contact</HashLink>
+                <HashLink to="/resume" className={highlight === 'resume' ? "highlightedNav": ""}>Resume</HashLink>
+                <a target="_blank" href="https://andrew-li-blog.vercel.app/">Blog</a>
+            </div>
         </nav>
     )
 }
