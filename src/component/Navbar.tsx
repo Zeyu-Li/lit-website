caf66ed251d7db37d285a65b593f849e97d917ea
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-15 21:00:45 -0600

    started navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,10 +1,17 @@
 import React, {useState, } from 'react'
+import logo from './../images/logo.png'
 
 
 export const Navbar: React.FC = () => {
 
     return (
-        <>
-        </>
+        <nav>
+            <a><img src={logo} />Andrew's Portfolio</a>
+            <a>Home</a>
+            <a>Projects</a>
+            <a>Contact</a>
+            <a>Resume</a>
+            <a>Blog</a>
+        </nav>
     )
 }
