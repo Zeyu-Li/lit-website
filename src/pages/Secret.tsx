b77e7560630b4cc31afc20fadb9669e8fa3eb3f0
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-15 20:37:21 -0600

    finished layout secrets

---
+++ src/pages/Secret.tsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { AiFillHome } from 'react-icons/ai'
+
+import { ReactComponent as PenguinCoin } from '../images/coin.svg'
+
+
+export const Secret: React.FC = () => {
+    // sets title
+    useEffect(() => {
+        document.title = "Andrew's Portfolio | 😳"
+    }, []);
+
+    return (
+        <>
+            <PenguinCoin />
+            <h1>Secret???</h1>
+            <p>Good job, you get a Penguin Bottom Coin</p>
+            <AiFillHome color="blue" /> <Link to="/"><a>Take me home</a></Link>
+        </>
+    )
+}
