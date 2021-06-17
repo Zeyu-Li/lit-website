e17866a935b496bb1ae4f7c22ee5715834b8afce
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-16 20:43:43 -0600

    started intro page

--- src/component/home/Intro.tsx
+++ src/component/home/Intro.tsx
@@ -1,11 +1,26 @@
-import React, {useState, } from 'react'
+import React, {useState, useEffect} from 'react'
+import { ReactComponent as PenguinCoin } from '../../images/coin.svg'
 
 
 export const Intro: React.FC = () => {
+    const [loaded, setLoad] = useState(false)
+
+    useEffect(()=> {
+        setLoad(true)
+    }, [])
 
     return (
         <>
-            
+        <div className="block singles">
+            <div className="intro">
+                <PenguinCoin className="pageIcons" />
+                <div className="pageText">
+                    <h1>Andrew Li</h1>
+                    <div className={`divider ${loaded ? "dividerLoaded" : ""}`} />
+                    <p className="singleText">Full Stack <wbr />and <wbr />Mobile Developer</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
