e126b9d12767244079518873eafa388a7a7ba621
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-10 07:10:49 -0600

    reformatted projects page + change project

--- src/component/home/Contact.tsx
+++ src/component/home/Contact.tsx
@@ -1,42 +1,65 @@
-import React, {useState, useEffect} from 'react'
-import { IoMdMail } from 'react-icons/io'
-import { AiFillLinkedin } from 'react-icons/ai'
-import { VscGithub } from 'react-icons/vsc'
+import React, { useState, useEffect } from "react";
+import { IoMdMail } from "react-icons/io";
+import { AiFillLinkedin } from "react-icons/ai";
+import { VscGithub } from "react-icons/vsc";
 
-import { Footer } from '../Footer'
-
+import { Footer } from "../Footer";
 
 interface Props {
-    highlight: string
+    highlight: string;
 }
 
-export const Contact: React.FC<Props> = React.memo(({highlight}) => {
-    const [iconSize, setIconSize] = useState(window.innerWidth > 800 ? 60 : 28)
+export const Contact: React.FC<Props> = React.memo(({ highlight }) => {
+    const [iconSize, setIconSize] = useState(window.innerWidth > 800 ? 60 : 28);
 
     // on resize
     useEffect(() => {
         const resizeListener = () => {
-            setIconSize(window.innerWidth > 800 ? 60 : 28)
-        }
-        
+            setIconSize(window.innerWidth > 800 ? 60 : 28);
+        };
+
         // set resize listener
-        window.addEventListener('resize', resizeListener)
-    }, [])
+        window.addEventListener("resize", resizeListener);
+    }, []);
 
     return (
         <>
-        <div id="contact" className="contact" style={{position: "relative"}}>
-            <div className="mainContact">
-                <h2>Contact</h2>
-                <a href="mailto:hello@andrewli.site" title="Email me"><span className="contactIcons"><IoMdMail size={iconSize} color="2EA2F8" /></span>hello@<wbr />andrewli.site</a><br/>
-                <a href="https://www.linkedin.com/in/andrew-li23/" title="Connect with me on LinkedIn" rel="noreferrer" target="_blank"><span className="contactIcons"><AiFillLinkedin size={iconSize} color="2EA2F8" /></span>andrew-li23</a><br/>
-                <a href="https://github.com/Zeyu-Li" title="Check my GitHub" rel="noreferrer" target="_blank"><span className="contactIcons"><VscGithub size={iconSize} color="2EA2F8" /></span>Zeyu-Li</a>
+            <div id="contact" className="contact" style={{ position: "relative" }}>
+                <div className="mainContact">
+                    <h2>Contact</h2>
+                    <a href="mailto:hello@andrewli.site" title="Email me">
+                        <span className="contactIcons">
+                            <IoMdMail size={iconSize} color="2EA2F8" />
+                        </span>
+                        hello@
+                        <wbr />
+                        andrewli.site
+                    </a>
+                    <br />
+                    <a
+                        href="https://www.linkedin.com/in/andrew-li23/"
+                        title="Connect with me on LinkedIn"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <span className="contactIcons">
+                            <AiFillLinkedin size={iconSize} color="2EA2F8" />
+                        </span>
+                        andrew-li23
+                    </a>
+                    <br />
+                    <a href="https://github.com/Zeyu-Li" title="Check my GitHub" rel="noreferrer" target="_blank">
+                        <span className="contactIcons">
+                            <VscGithub size={iconSize} color="2EA2F8" />
+                        </span>
+                        Zeyu-Li
+                    </a>
+                </div>
+
+                <div className="contactFooter">
+                    <Footer highlight={highlight} />
+                </div>
             </div>
-            
-            <div className="contactFooter">
-                <Footer highlight={highlight} />
-            </div>
-        </div>
         </>
-    )
-})
+    );
+});
--- src/component/home/Projects.tsx
+++ src/component/home/Projects.tsx
@@ -1,77 +1,147 @@
-import React from 'react'
-import { Parallax } from 'react-parallax'
-import { VscGithub } from 'react-icons/vsc'
-import { MdWeb } from 'react-icons/md'
-import { FaGamepad } from 'react-icons/fa'
+import React from "react";
+import { Parallax } from "react-parallax";
+import { VscGithub } from "react-icons/vsc";
+import { MdWeb } from "react-icons/md";
+import { FaGamepad } from "react-icons/fa";
 
+interface Props {
+    title: string;
+    description: any;
+    github?: string;
+    githubTitle?: string;
+    demo?: string;
+    demoTitle?: string;
+    image?: string;
+    imageAlt?: string;
+    imageStrength?: number;
+    isGame?: boolean;
+}
+
+const Project: React.FC<Props> = ({
+    title,
+    github,
+    githubTitle,
+    demo,
+    demoTitle,
+    image,
+    imageAlt,
+    description,
+    imageStrength = 300,
+    isGame = false,
+}) => {
+    return (
+        <div className="singleProject">
+            <h4>{title}</h4>
+            {image ? (
+                <Parallax bgImage={image} bgImageAlt={imageAlt} strength={imageStrength} style={{ width: "70vw" }}>
+                    <div style={{ height: "44vw" }}></div>
+                </Parallax>
+            ) : null}
+            {description ?? null}
+            {github ? (
+                <>
+                    <a href={github} title={githubTitle} rel="noreferrer" target="_blank">
+                        <span className="contactIcons">
+                            <VscGithub size={32} color="2EA2F8" />
+                        </span>
+                        Source code
+                    </a>
+                    <br />
+                </>
+            ) : null}
+
+            {demo ? (
+                <a href={demo} title={demoTitle} rel="noreferrer" target="_blank">
+                    <span className="contactIcons">
+                        {isGame ? <FaGamepad size={32} color="2EA2F8" /> : <MdWeb size={32} color="2EA2F8" />}
+                    </span>
+                    Demo
+                </a>
+            ) : null}
+        </div>
+    );
+};
 
 export const Projects: React.FC = React.memo(() => {
-    const repoCount = 150
-    // TODO: generate project component
+    const repoCount = 150;
 
     return (
         <>
             <div id="projects" className="projects">
                 <h2>Projects</h2>
-                <div className="singleProject">
-                    <h4>Chatter</h4>
-                    <Parallax  bgImage={"chatter.png"} bgImageAlt="chatter screenshots" strength={300} style={{width: "70vw"}}>
-                        {/* <Background className="custom-bg">
-                            <img alt="chatter screenshots" src={"chatter.png"} />
-                        </Background> */}
-                        <div style={{height: "44vw"}}></div>
-                    </Parallax>
-                    <p>
-                        A realtime chat website
-                        <br />{'>'} Using React, Firebase, JavaScript
-                    </p>
-                    <a href="https://github.com/Zeyu-Li/chatter" title="Chatter repo link (GitHub)" rel="noreferrer" target="_blank"><span className="contactIcons"><VscGithub size={32} color="2EA2F8" /></span>Source code</a><br />
-                    <a href="https://chatter-zeyu-li.vercel.app/" title="Chatter Project Demo" rel="noreferrer" target="_blank"><span className="contactIcons"><MdWeb size={32} color="2EA2F8" /></span>Demo</a>
-                </div>
-                <div className="singleProject">
-                    <h4 style={{paddingBottom: 0}}>Lua Runner</h4>
-                    <p>
-                        A NPM Typescript module that runs Lua with over <b>1,800 downloads</b> 
-                        <br />{'>'} Using TypeScript, Webassembly
-                    </p>
-                    <a href="https://github.com/Zeyu-Li/lua-runner" title="Lua Runner source code" rel="noreferrer" target="_blank"><span className="contactIcons"><VscGithub size={32} color="2EA2F8" /></span>Source code</a><br />
-                    <a href="https://www.npmjs.com/package/lua-runner" title="Check NPM page" rel="noreferrer" target="_blank"><span className="contactIcons"><MdWeb size={32} color="2EA2F8" /></span>Demo</a>
-                </div>
-                <div className="singleProject">
-                    <h4>Django Ecommerce</h4>
-                    <Parallax bgImage={"django.png"} bgImageAlt="django screenshots" strength={300} style={{width: "70vw"}}>
-                        {/* <Background className="custom-bg">
-                            <img alt="django screenshots" src={"django.png"} />
-                        </Background> */}
-                        <div style={{height: "44vw"}}></div>
-                    </Parallax>
-                    <p>
-                        An ecommerce website build on Django and Bootstrap with login and shopping cart (connected to Stripe)
-                        <br />{'>'} Using Python (Django), Stripe, JavaScript, CSS, HTML, Bootstrap
-                    </p>
-                    <a href="https://github.com/Zeyu-Li/chatter" title="Django source code on GitHub" rel="noreferrer" target="_blank"><span className="contactIcons"><VscGithub size={32} color="2EA2F8" /></span>Source code</a><br />
-                </div>
-                <div className="singleProject" style={{paddingBottom: "5vh"}}>
-                    <h4>Advanced Unity Tutorial</h4>
-                    <Parallax  bgImage={"game.png"} bgImageAlt="game screen" strength={150} style={{width: "70vw"}}>
-                        {/* <Background className="custom-bg">
-                            <img alt="django screenshots" src={"django.png"} />
-                        </Background> */}
-                        <div style={{height: "44vw"}}></div>
-                    </Parallax>
-                    <p>
-                        An Advanced Unity Tutorial, part of a larger Unity Tutorial series. Play demo is found below. 
-                        <br />{'>'} Using C#, Blender, Adobe Illustator, Unity 
-                    </p>
-                    <a href="https://github.com/Zeyu-Li/Unity-Advanced-3D-Tutorial" title="Unity Advanced Tutorial source code on GitHub" rel="noreferrer" target="_blank"><span className="contactIcons"><VscGithub size={32} color="2EA2F8" /></span>Source code</a><br />
-                    <a href="https://andrewli.itch.io/unity-advanced-tutorial" title="Game demo" rel="noreferrer" target="_blank"><span className="contactIcons"><FaGamepad size={32} color="2EA2F8" /></span>Demo</a><br />
-                </div>
+                <Project
+                    title="Chatter"
+                    description={
+                        <p>
+                            A realtime chat website
+                            <br />
+                            {">"} Using React, Firebase, JavaScript
+                        </p>
+                    }
+                    image="chatter.png"
+                    imageAlt="chatter screenshot"
+                    github="https://github.com/Zeyu-Li/chatter"
+                    githubTitle="Chatter repo link (GitHub)"
+                    demo="https://chatter-zeyu-li.vercel.app/"
+                    demoTitle="Chatter Project Demo"
+                />
+                <Project
+                    title="Lua Runner"
+                    description={
+                        <p>
+                            A NPM Typescript module that runs Lua with over <b>2,000 downloads</b>
+                            <br />
+                            {">"} Using TypeScript, Webassembly
+                        </p>
+                    }
+                    github="https://github.com/Zeyu-Li/lua-runner"
+                    githubTitle="Lua Runner source code"
+                    demo="https://www.npmjs.com/package/lua-runner"
+                    demoTitle="Lua Runner NPM page"
+                />
+                <Project
+                    title="Django Ecommerce"
+                    description={
+                        <p>
+                            An ecommerce website build on Django and Bootstrap with login and shopping cart (connected
+                            to Stripe)
+                            <br />
+                            {">"} Using Python (Django), Stripe, JavaScript, CSS, HTML, Bootstrap
+                        </p>
+                    }
+                    image="django.png"
+                    imageAlt="django website screenshots"
+                    github="https://github.com/Zeyu-Li/django_ecommerce_app"
+                    githubTitle="Django source code on GitHub"
+                />
+                <Project
+                    title="Attacked from Above"
+                    description={
+                        <p>
+                            An atmospheric isometric game with aliens. Play demo now online at the link below.
+                            <br />
+                            {">"} Using C#, Blender, Adobe Illustrator, Unity
+                        </p>
+                    }
+                    image="game.png"
+                    imageAlt="game screenshot"
+                    imageStrength={150}
+                    github="https://github.com/Zeyu-Li/innovator-jam-2021"
+                    githubTitle="Game source code"
+                    demo="https://andrewli.itch.io/attacked-from-above"
+                    demoTitle="Game demo"
+                    isGame
+                />
+                <br />
                 <div className="endOfProjects">
                     <p>
-                        Over {repoCount} projects/repos on <a href="https://github.com/Zeyu-Li" rel="noreferrer" target="_blank">GitHub</a>
+                        Over {repoCount} projects/repos on{" "}
+                        <a href="https://github.com/Zeyu-Li" rel="noreferrer" target="_blank">
+                            GitHub
+                        </a>
                     </p>
                 </div>
             </div>
         </>
-    )
-})
+    );
+});
