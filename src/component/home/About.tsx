38a98de387df56e1bc9d5053809fe7737a719c7b
Andrew Li <zeyu.li23@gmail.com>
2022-05-28 10:51:32 -0600

    updated role

--- src/component/home/About.tsx
+++ src/component/home/About.tsx
@@ -1,50 +1,55 @@
-import React, {useRef, useState} from 'react'
-import { HashLink } from 'react-router-hash-link'
-import { useIntersection } from 'react-use'
-import {gsap} from 'gsap'
+import React, { useRef, useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import { useIntersection } from "react-use";
+import { gsap } from "gsap";
 
-import { Typing } from './Typing'
-
+import { Typing } from "./Typing";
 
 export const About: React.FC = React.memo(() => {
-    const sectionRef = useRef(null)
-    const header = useRef(null)
-    const [active, setActive] = useState(false)
+    const sectionRef = useRef(null);
+    const header = useRef(null);
+    const [active, setActive] = useState(false);
     const intersection = useIntersection(sectionRef, {
         root: null,
         rootMargin: "0px",
-        threshold: 0.5
-    })
+        threshold: 0.5,
+    });
     const showAnimation = () => {
-        setActive(true)
+        setActive(true);
         // animation (fade in)
-        gsap.fromTo(header.current, {
-            duration: 1,
-            autoAlpha: 0,
-            rotationX: 80,
-            transformOrigin: '50% 50% -40px',
-            ease: 'power3.easeOut',
-        }, {
-            autoAlpha: 1,
-            rotationX: 0,
-            ease: 'power3.easeOut',
-        })
-    }
+        gsap.fromTo(
+            header.current,
+            {
+                duration: 1,
+                autoAlpha: 0,
+                rotationX: 80,
+                transformOrigin: "50% 50% -40px",
+                ease: "power3.easeOut",
+            },
+            {
+                autoAlpha: 1,
+                rotationX: 0,
+                ease: "power3.easeOut",
+            }
+        );
+    };
 
-    if (intersection && intersection.intersectionRatio > 0.5 && !active) showAnimation()
+    if (intersection && intersection.intersectionRatio > 0.5 && !active) showAnimation();
 
     return (
         <div id="about" className="block white">
             <div className="about" ref={sectionRef}>
-                <h2 className="header1" ref={header}>About Me</h2>
+                <h2 className="header1" ref={header}>
+                    About Me
+                </h2>
                 <h3 className="singleText type">I can code {active ? <Typing /> : ""}</h3>
                 <p>
-                    Currently interning at Intuit and working at Zero RampUp as a <b>full stack dev</b>. 
-                    Formerly a <b>research assistant</b> at the University of Alberta. 
-                    <wbr/> If you want to add me on your team or just want to chat, 
-                    click on <HashLink to="/#contact" >Contact</HashLink>
+                    Currently interning at Intuit and working at Zero RampUp as a <b>project manager</b>. Formerly a{" "}
+                    <b>research assistant</b> at the University of Alberta.
+                    <wbr /> If you want to add me on your team or just want to chat, click on{" "}
+                    <HashLink to="/#contact">Contact</HashLink>
                 </p>
             </div>
         </div>
-    )
-})
+    );
+});
